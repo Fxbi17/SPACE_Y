e10d4953aaa4f181d3535cde148ac334499d323f
Fabio Da Silva <fabio.dsv@hotmail.com>
2023-01-28 11:02:23 +0100

    lolilol

--- app/models/spaceship.rb
+++ app/models/spaceship.rb
@@ -1,6 +1,7 @@
 class Spaceship < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  has_one_attached :image
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
